39c48639d1057a846f032eb7623f73494fd9bc0c
patryk_s <boydmalvo@gmail.com>
2021-12-20 18:53:03 +0100

    owner, balance, number, transfer

--- electronic_devices/atm.py
+++ electronic_devices/atm.py
@@ -1,14 +1,28 @@
 class Account:
-    def __init__(self,account_number, name, surname, account_state = 0,debt =0):
+    def __init__(self, account_number, name, surname, account_state = 0,debt =0):
         self.account_number = account_number
         self.name = name
         self.surname = surname
         self.account_state = account_state
         self.debt = debt
-        print(account_number)
+
 
     def owner(self):
          return self.name, self.surname
+
+    def balance(self):
+        return self.account_state
+
+    def number(self):
+        return self.account_number
+
+    def transfer(self, deposit):
+        if (self.account_state + deposit) >= 0:
+            self.account_state += deposit
+            return self.account_state
+        else:
+            return "You have no money!"
+
 
 
 class Card(Account):
@@ -16,6 +30,8 @@ class Card(Account):
         self.pin = pin
 
 
-# s1 = Account(123321,"Bob","Marley",1000,0)
-#
-# Account.owner(s1)
+s1 = Account(123321, "Bob", "Marley", 1000, 0)
+s1.transfer(300)
+s1.transfer(-200)
+s1.transfer(-1300)
+print(Account.balance(s1))
--- tests/test_atm.py
+++ tests/test_atm.py
@@ -16,8 +16,21 @@ def card():
 # def test_account_print_accpunt_number(acc):
 #     assert acc.account_number == acc.account_number
 
-@pytest.mark.parametrize("given, expected",[((),"Bob Marley")])
-def test_acc_owner(given,expected,acc):
-    assert acc.owner(given) == expected
+@pytest.mark.parametrize("given, expected",[((acc),('Bob', 'Marley'))])
+def test_acc_owner_return_name_and_surname(given,expected,acc):
+    assert acc.owner() == expected
 
+@pytest.mark.parametrize("given, expected",[((acc),1000)])
+def test_acc_balance_return_account_state(given,expected,acc):
+    assert acc.balance() == expected
 
+@pytest.mark.parametrize("given, expected",[((acc),123321)])
+def test_acc_owner_return_acc_number(given,expected,acc):
+    assert acc.number() == expected
+
+def test_acc_owner_return_acc_number(acc):
+    assert acc.number() == 123321
+
+@pytest.mark.parametrize("given, expected",[(100,1100),(-1000,0),(-1010,"You have no money!")])
+def test_acc_transfer_works(given,expected,acc):
+    assert acc.transfer(deposit=given) == expected
