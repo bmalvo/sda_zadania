b0a8d16f65287bd91840d62763ed5fe9a8a898f5
patryk_s <boydmalvo@gmail.com>
2021-12-20 16:25:34 +0100

    commit before merge

--- electronic_devices/atm.py
+++ electronic_devices/atm.py
@@ -1,11 +1,14 @@
 class Account:
-    def __init__(self,account_number, name, surname, account_state = 0,debt =0):
+    def __init__(self,account_number, name, surname, account_state=0, debt=0):
         self.account_number = account_number
         self.name = name
         self.surname = surname
         self.account_state = account_state
         self.debt = debt
-        print(account_number)
+
+    def __str__(self):
+        return f"{self.account_number}".format(self=self)
+
 
     def owner(self):
          return self.name, self.surname
@@ -16,6 +19,7 @@ class Card(Account):
         self.pin = pin
 
 
-# s1 = Account(123321,"Bob","Marley",1000,0)
-#
-# Account.owner(s1)
+s1 = Account(123321, "Bob", "Marley", 1000, 0)
+
+Account.owner(s1)
+print(str(s1))
