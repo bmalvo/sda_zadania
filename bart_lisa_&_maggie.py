1a728ad6c41e635ced7ebb416cfc8bef03a2e4d1
patryk_s <boydmalvo@gmail.com>
2021-12-30 12:33:54 +0100

    Bart, Lisa & Maggie

--- bart_lisa_&_maggie.py
+++ bart_lisa_&_maggie.py
@@ -9,15 +9,19 @@
 # namelist([ {'name': 'Bart'} ])
 # # returns 'Bart'
 
-names_4 = [{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}, {'name': 'Stefka'}]
+
+names_4 = [{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}, {'name': 'Stefka'},{'name': 'Kulfon'}]
 names_3 = [{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]
 names_2 = [{'name': 'Bart'}, {'name': 'Lisa'}]
 names_1 = [{'name': 'Bart'}]
+names_0 = []
 
 
-def list_of_names(names):
+
+def namelist(names):
     names = [x["name"] for x in names]
-    print(names)
+    if len(names) == 0:
+        return ''
     if len(names) == 1:
         return names[0]
     if len(names) == 2:
@@ -31,8 +35,19 @@ def list_of_names(names):
         names_first = ", ".join(names_first)
         names_second = " ".join(names_second)
         names = names_first + " " + names_second
-
+    if len(names) > 3:
+        names.insert(-1,'&')
+        names_first = names[:-2]
+        names_second = names[-2:]
+        names_first = ", ".join(names_first)
+        names_second = " ".join(names_second)
+        names = names_first + " " + names_second
         return names
 
 
-print(list_of_names(names_3))
+print(namelist(names_4))
+
+def namelist1(names):
+  return ", ".join([name["name"] for name in names])[::-1].replace(",", "& ",1)[::-1]
+
+print(namelist1(names_4))
