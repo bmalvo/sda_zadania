368e7e1c74ba78f6b801f0ca5f903945d0ac8ac9
patryk_s <boydmalvo@gmail.com>
2022-08-17 12:10:52 +0200

    improve count_pos_sum_negative

--- codewars/kata_8/tests/test_count_pos_sum_negative.py
+++ codewars/kata_8/tests/test_count_pos_sum_negative.py
@@ -4,3 +4,13 @@ from codewars.kata_8.files.count_pos_sum_negative import count_positives_sum_neg
 def test_count_positives_sum_negatives():
     testing_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
     assert count_positives_sum_negatives(testing_list) == [10, -65]
+
+
+def test_count_positives_sum_negatives_if_zer_zero():
+    testing_list = [0, 0]
+    assert count_positives_sum_negatives(testing_list) == [0, 0]
+
+
+def test_count_positives_sum_negatives_if_null():
+    testing_list = []
+    assert count_positives_sum_negatives(testing_list) == []
