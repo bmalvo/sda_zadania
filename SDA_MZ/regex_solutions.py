f86d5318064d280f6558244f8f954600c3096dd4
patryk_s <boydmalvo@gmail.com>
2022-09-16 14:33:01 +0200

    task 1 regex

---
+++ SDA_MZ/regex_solutions.py
@@ -0,0 +1,21 @@
+"""Here are answers to regex_task.txt"""
+
+# task 1
+
+
+# def checking_input():
+#     """ Checking input characteristics"""
+#     number = input("Type some number: ")
+#     if not number.isdigit():
+#         print(f'Your input:"{number}" is not a integer. Try again: ')
+#         checking_input()
+#     else:
+#         even_or_odd = (int(number) % 2 == 0)
+#         if even_or_odd:
+#             res = f'Your number: {number} is even'
+#         else:
+#             res = f'Your number: {number} is odd'
+#         return res
+#
+#
+# print(checking_input())
