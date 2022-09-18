06dd53fe699bb34eb79f2c5695e402bfeaaed4aa
patryk_s <boydmalvo@gmail.com>
2022-09-18 13:37:52 +0200

    tests login_validator

--- SDA_MZ/tests_regex.py
+++ SDA_MZ/tests_regex.py
@@ -1,5 +1,5 @@
 import pytest
-from SDA_MZ.regex_solutions import code_validator
+from SDA_MZ.regex_solutions import code_validator, login_validator
 
 
 def test_code_validator_1():
@@ -19,3 +19,11 @@ def test_code_validator_failed():
     if code_validator(22-222):
         pytest.xfail('input should be string')
 
+
+def test_login_validation_valid():
+    assert login_validator('BillyJoe33') == 'This is a valid login.'
+
+
+def test_login_validation_invalid():
+    assert login_validator('short') == 'This login is invalid.'
+    
