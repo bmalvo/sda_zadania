2196e5f6acdc60757e109f6b2f3b55ed975973eb
patryk_s <boydmalvo@gmail.com>
2022-09-19 18:08:27 +0200

    gen_it task 1

---
+++ SDA_MZ/genrators_iterators.py
@@ -0,0 +1,14 @@
+# task 1
+
+cats = {
+    'dąbrowskiego': ['Stefka', 'Brydzia'],
+    'wierzbowa': 'Ninja',
+    'zawoja': 'Drapek',
+    'cygany': 'Zara'
+}
+
+for street, cat in cats.items():
+    print(street)
+
+for street, cat in cats.items():
+    print(cat)
