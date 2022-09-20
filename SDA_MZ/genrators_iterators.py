7ece1039c760feac0b2b4541521f5fc98fa5b5e0
patryk_s <boydmalvo@gmail.com>
2022-09-20 17:36:06 +0200

    task 2 gen_ite

--- SDA_MZ/genrators_iterators.py
+++ SDA_MZ/genrators_iterators.py
@@ -1,14 +1,28 @@
 # task 1
 
-cats = {
-    'dąbrowskiego': ['Stefka', 'Brydzia'],
-    'wierzbowa': 'Ninja',
-    'zawoja': 'Drapek',
-    'cygany': 'Zara'
-}
 
-for street, cat in cats.items():
-    print(street)
+def iterator_ex():
+    cats = {
+        'dąbrowskiego': ['Stefka', 'Brydzia'],
+        'wierzbowa': 'Ninja',
+        'zawoja': 'Drapek',
+        'cygany': 'Zara'
+    }
 
-for street, cat in cats.items():
-    print(cat)
+    for street, cat in cats.items():
+        print(street)
+
+    for street, cat in cats.items():
+        print(cat)
+
+
+# task 2
+
+
+def numbers(n):
+    lst_numb = []
+    for i in range(n):
+        lst_numb.append(i)
+    return sum(lst_numb)
+
+
