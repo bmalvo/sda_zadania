a97e4b24d0190cb992495d01353bea905cc4799e
patryk_s <boydmalvo@gmail.com>
2022-09-22 20:29:49 +0200

    task 2 lambda

--- SDA_MZ/lambda_solutions.py
+++ SDA_MZ/lambda_solutions.py
@@ -22,3 +22,24 @@ def sort_first_letter_asc():
     return nameList
 
 
+# task 2
+
+
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+
+aron = BankAccount('Aron', 1000)
+bill = BankAccount('Bill', 4000)
+cecylia = BankAccount('Cecylia', 5000)
+david = BankAccount('David', 7500)
+egon = BankAccount('Egon', 10000)
+
+
+account_list = [aron, bill, cecylia, david, egon]
+
+big_account = filter(lambda x: x.balance > 4500, account_list)
+
+print([x.name for x in big_account])
