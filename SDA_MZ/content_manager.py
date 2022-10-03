a3232d3cdb71576c69261cd08d3eea2664491aaf
patryk_s <boydmalvo@gmail.com>
2022-10-03 18:56:13 +0200

    task 2 content_manager

--- SDA_MZ/content_manager.py
+++ SDA_MZ/content_manager.py
@@ -9,3 +9,37 @@ def reader(file):
 
 
 print(next(reader(r'inwokacja')))
+
+# task 2:
+
+
+class ReadFile:
+
+    def __init__(self, path=''):
+        self.__source = path
+
+    @property
+    def source(self):
+        return self.__source
+
+    @source.setter
+    def source(self, path: str):
+        self.__source = path
+
+    def __enter__(self):
+        self.__file_descriptor = open(self.source, 'a')
+        return self.__file_descriptor
+
+    def __exit__(self, *args):
+        self.__file_descriptor.close()
+
+
+def write_file_with_object(path='for_test.txt', text='abc'):
+    try:
+        with ReadFile(path) as rf:
+            rf.write(text)
+    except IOError as e:
+        print(f'IOError caught {e.args}')
+
+
+write_file_with_object()
