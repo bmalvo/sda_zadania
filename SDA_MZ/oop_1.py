b177b28c221ec827a34ede20578d843779ec1426
patryk_s <boydmalvo@gmail.com>
2022-10-31 18:42:57 +0100

    task 7 oop_1

--- SDA_MZ/oop_1.py
+++ SDA_MZ/oop_1.py
@@ -3,35 +3,35 @@ from abc import ABC, abstractmethod
 # task 1,3
 
 
-class Cat:
-
-    def __init__(self, name='unnamed_cat'):
-        self.name = name
-        self.meal = {
-            'mouse': 0
-        }
-
-    def __repr__(self):
-        return f'Cat - {self.name}'
-
-    def make_sound(self):
-        return f'{self.name}: Meooow!'
-
-    def eat_mouse(self):
-
-        self.meal['mouse'] += 1
-        mice = self.meal['mouse']
-        return f'{self.name} ate {mice} mice'
+# class Cat:
+#
+#     def __init__(self, name='unnamed_cat'):
+#         self.name = name
+#         self.meal = {
+#             'mouse': 0
+#         }
+#
+#     def __repr__(self):
+#         return f'Cat - {self.name}'
+#
+#     def make_sound(self):
+#         return f'{self.name}: Meooow!'
+#
+#     def eat_mouse(self):
+#
+#         self.meal['mouse'] += 1
+#         mice = self.meal['mouse']
+#         return f'{self.name} ate {mice} mice'
 
 # task 2
 
 
 # cats_list = []
 
-cat1 = Cat('Ninja')
-cat2 = Cat('Zara')
-cat3 = Cat('Drapek')
-cat4 = Cat('Brydzia')
+# cat1 = Cat('Ninja')
+# cat2 = Cat('Zara')
+# cat3 = Cat('Drapek')
+# cat4 = Cat('Brydzia')
 # cats_list.append(cat1)
 # cats_list.append(cat2)
 # cats_list.append(cat3)
@@ -73,7 +73,7 @@ dog4 = Dog('Sindy')
 # task 5
 
 
-animals = [cat1, cat2, cat3, cat4, dog1, dog2, dog3, dog4]
+# animals = [cat1, cat2, cat3, cat4, dog1, dog2, dog3, dog4]
 
 # for animal in animals:
 #     print(animal.make_sound())
@@ -92,6 +92,35 @@ class Car(Movable):
     def move(self):
         return f'I drive'
 
+# task 7
+
+
+class Cat(Movable):
+
+    def __init__(self, name='unnamed_cat'):
+        self.name = name
+        self.meal = {
+            'mouse': 0
+        }
+
+    def __repr__(self):
+        return f'Cat - {self.name}'
+
+    def make_sound(self):
+        return f'{self.name}: Meooow!'
+
+    def eat_mouse(self):
+
+        self.meal['mouse'] += 1
+        mice = self.meal['mouse']
+        return f'{self.name} ate {mice} mice'
+
+    def move(self):
+        return f'I sneak'
+
 
 ferrari = Car()
 print(ferrari.move())
+fifi = Cat('fifi')
+print(fifi)
+print(fifi.move())
