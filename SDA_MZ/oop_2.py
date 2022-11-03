25b91d6b3177c84f5dd77d4b8ab9a8eae0756a43
patryk_s <boydmalvo@gmail.com>
2022-11-03 17:57:25 +0100

    task 2 oop_2

--- SDA_MZ/oop_2.py
+++ SDA_MZ/oop_2.py
@@ -1,9 +1,40 @@
-# task 1
+import datetime
+
+# task 1, 2
+
 
 class Person:
 
-    def __init__(self, name, surname, birth_date):
-        self.name = name
-        self.surname = surname
-        self.birth_date = birth_date
-        
+    def __init__(self, name, surname, birth_year):
+        self._name = name
+        self._surname = surname
+        self._birth_year = birth_year
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def surname(self):
+        return self._surname
+
+    @surname.setter
+    def surname(self, value):
+        self._surname = value
+
+    @property
+    def birth_year(self):
+        return self._birth_year
+
+    @birth_year.setter
+    def birth_year(self, year):
+        current_year = datetime.datetime.now().year
+        if current_year < year:
+            raise ValueError('Are you from future? So come back there!')
+        else:
+            self._birth_year = year
+
