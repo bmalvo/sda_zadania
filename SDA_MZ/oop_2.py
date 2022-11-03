f70f295de429d3374dc652b1f0b17c5321786bd2
patryk_s <boydmalvo@gmail.com>
2022-11-03 16:38:05 +0100

    task 1 oop_2

---
+++ SDA_MZ/oop_2.py
@@ -0,0 +1,9 @@
+# task 1
+
+class Person:
+
+    def __init__(self, name, surname, birth_date):
+        self.name = name
+        self.surname = surname
+        self.birth_date = birth_date
+        
