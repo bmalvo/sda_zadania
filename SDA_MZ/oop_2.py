1ae20c5b9e07bef2ebc75e21269646f9d392960e
patryk_s <boydmalvo@gmail.com>
2022-11-04 16:50:24 +0100

    task 4 oop_2

--- SDA_MZ/oop_2.py
+++ SDA_MZ/oop_2.py
@@ -5,7 +5,7 @@ import datetime
 
 class Person:
 
-    def __init__(self, name, surname, birth_year):
+    def __init__(self, name='Noname', surname="Anonim", birth_year=1900):
         self._name = name
         self._surname = surname
         self._birth_year = birth_year
@@ -33,13 +33,26 @@ class Person:
     @birth_year.setter
     def birth_year(self, year):
         current_year = datetime.datetime.now().year
-        if current_year < year:
+        print(current_year)
+        if current_year < int(year):
             raise ValueError('Are you from future? So come back there!')
         else:
             self._birth_year = year
 
-# task 3
+# task 3, 4
 
 
 class Employee(Person):
-    pass
+
+    @Person.birth_year.setter
+    def birth_year(self, value):
+        if value < 1900 or value > 2020:
+            self._birth_year = 0
+        else:
+            self._birth_year = value
+
+
+# John = Employee()
+# John.birth_year = 1900
+# print(John.birth_year)
+# assert John.birth_year == 1900
