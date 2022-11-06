56ecbf054d8f31de7743953b0cceafc51d8ee59d
patryk_s <boydmalvo@gmail.com>
2022-11-06 17:53:25 +0100

    task 5 oop_2

--- SDA_MZ/oop_2.py
+++ SDA_MZ/oop_2.py
@@ -44,6 +44,18 @@ class Person:
 
 class Employee(Person):
 
+    def __init__(self, name, surname, birth_date, salary=0):
+        super().__init__(name, surname, birth_date)
+        self._salary = salary
+
+    @property
+    def salary(self):
+        return self._salary
+
+    @salary.setter
+    def salary(self, value):
+        self._salary = value
+
     @Person.birth_year.setter
     def birth_year(self, value):
         if value < 1900 or value > 2020:
@@ -51,8 +63,16 @@ class Employee(Person):
         else:
             self._birth_year = value
 
+    def who_am_i(self):
+        return f'My name is {self.name}-{self.surname} and my salary is: {self.salary}$'
 
-# John = Employee()
-# John.birth_year = 1900
-# print(John.birth_year)
-# assert John.birth_year == 1900
+
+John = Employee('John', "Rambo", 1954)
+print(John.birth_year)
+assert John.birth_year == 1954
+print(John.name)
+print(John.surname)
+print(John.salary)
+John.salary = 13000
+print(John.salary)
+print(John.who_am_i())
