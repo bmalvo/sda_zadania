2ba70055e9af2e949c6c70d2836643e86b42ffad
patryk_s <boydmalvo@gmail.com>
2022-11-13 15:40:08 +0100

    task 3 oop_3

--- SDA_MZ/oop_3.py
+++ SDA_MZ/oop_3.py
@@ -9,3 +9,35 @@ week_days = {
     6: 'saturday',
     7: 'sunday'
 }
+
+# task 3
+
+
+class Complex:
+
+    def __init__(self, reality, imaginary=0):
+        self.reality = reality
+        self.imaginary = imaginary
+
+    def show(self):
+        sign = '+'
+        if self.reality == 0:
+            reality = ""
+        else:
+            reality = self.reality
+        if self.imaginary == 1:
+            imaginary = 'i'
+        elif self.imaginary == 0:
+            sign = ''
+            imaginary = ''
+        elif self.imaginary < 0:
+            sign = '-'
+            imaginary = f'{abs(self.imaginary)}i'
+        else:
+            imaginary = f'{self.imaginary}i'
+        result = f'{reality} {sign} {imaginary}'
+        return result
+
+
+numb = Complex(6, -4)
+print(numb.show())
